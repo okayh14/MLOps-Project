e3965871cc3c6d85ad5c5797b06b4dc39b5cdb27
Michelle Binder <mbinder1@smail.uni-koeln.de>
2025-03-17 10:23:56 +0100

    Reformatting

--- backend/data_service/services/data_preparation.py
+++ backend/data_service/services/data_preparation.py
@@ -2,28 +2,39 @@ import pandas as pd
 
 
 def remove_null_values(df):
-    """Removes rows containing null values."""
+    """
+    Removes rows containing null values.
+    """
     return df.dropna()
 
 
 def remove_duplicates(df):
-    """Removes duplicate rows."""
+    """
+    Removes duplicate rows.
+    """
     return df.drop_duplicates()
 
 
 def standardize_column_names(df):
-    """Converts column names to lowercase and replaces spaces with underscores."""
+    """
+    Converts column names to lowercase and replaces spaces with underscores.
+    """
     df.columns = df.columns.str.lower().str.replace(" ", "_")
     return df
 
 
 def remove_patient_id(df):
-    """Removes the Patient ID column if it exists."""
+    """
+    Removes the Patient ID column if it exists.
+    """
     return df.drop(columns=["patient_id"], errors="ignore")
 
 
 def split_blood_pressure(df):
-    """Splits the Blood Pressure column into Systolic and Diastolic BP if it exists and is a string."""
+    """
+    Splits the Blood Pressure column into Systolic and 
+    Diastolic BP if it exists and is a string.
+    """
     if "blood_pressure" in df.columns and df["blood_pressure"].dtype == object:
         split_values = df["blood_pressure"].str.split("/", expand=True)
 
@@ -41,7 +52,9 @@ def split_blood_pressure(df):
 
 
 def data_preparation(df: pd.DataFrame):
-    """Processes and returns the cleaned data as a DataFrame."""
+    """
+    Processes and returns the cleaned data as a DataFrame.
+    """
     df = remove_null_values(df)
     df = remove_duplicates(df)
     df = standardize_column_names(df)
